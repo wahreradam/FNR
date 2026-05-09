51880a88346a7d6ff76efb0a0bd0a07022011517
wahreradam <ture@sociocon.com>
2026-05-09 15:43:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const PASSWORD = "Figger Noid";
 const users = [
   { name: "Max", img: "https://via.placeholder.com/300", bio: "Cooler Typ" },
   { name: "Leon", img: "https://via.placeholder.com/300", bio: "Sportlich" },
-  { name: "#3 Julius", img: "https://via.placeholder.com/300", bio: "heightmaxxed, hltn, gymskipper, primal" },
-  { name: "#4 Naveen", img: "https://via.placeholder.com/300", bio: "currymaxxed, manlet, gymmaxxer" },
+  { name: "#3 Julius", img: "JuliusRanking.jpeg", bio: "heightmaxxed, hltn, gymskipper, primal" },
+  { name: "#4 Naveen", img: "NaveenRanking.jpeg", bio: "currymaxxed, manlet, gymmaxxer" },
   { name: "Paul", img: "https://via.placeholder.com/300", bio: "Smart" },
   { name: "Lukas", img: "https://via.placeholder.com/300", bio: "Chill" },
   { name: "Finn", img: "https://via.placeholder.com/300", bio: "Energie" },
