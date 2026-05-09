3c2b953fde1a4392f129731340671ee3aa2a810a
wahreradam <ture@sociocon.com>
2026-05-09 16:18:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const PASSWORD = "123";
 
 const users = [
   { name: "#1-#2 Ture", img: "TureRankings.jpeg", bio: "gymcel, primal, mtn" },
-  { name: "#1-#2 Nick", "NickRankings.jpeg", bio: "mtn, akkutanmaxxed" },
+  { name: "#1-#2 Nick", img: "NickRankings.jpeg", bio: "mtn, akkutanmaxxed" },
   { name: "#3 Julius", img: "JuliusRanking.jpeg", bio: "heightmaxxed, hltn, gymskipper, trueprimal, blackpilled, orgmaxxed" },
   { name: "#4 Naveen", img: "NaveenRanking.jpeg", bio: "currymaxxed, manlet, gymmaxxer, bluepilled" },
   { name: "#5-#6 Johann", img: "JohannRankings.jpeg", bio: "blackpilled, orgmaxxed, akkutanmaxxed" },
